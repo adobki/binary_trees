9f5249494f43a0436b6e20a560124ce09192a039
Donald Ajaps <ajapon@yahoo.com>
2023-03-01 11:22:27 +0100

    Do Task 0: New Node and add Pretty-Printer to repo
    Create header file for project (binary_trees.h) and do first task in project, 0.New Node: 0-binary_tree_node.c, tests\0-main.c.
    Also add given binary tree pretty-print function to tests folder for use with tests (*-main.c files).

---
+++ 0-binary_tree_node.c
@@ -0,0 +1,26 @@
+#include "binary_trees.h"
+
+/**
+ * create - Creates a binary tree node without any child.
+ *
+ * @parent: Pointer to the parent node of the node to create.
+ * @value: Data to be put in the new node.
+ *
+ * Return: Pointer to created node on success,
+ *         NULL otherwise.
+ */
+
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
+{
+	binary_tree_t *node = NULL;
+
+	node = malloc(sizeof(binary_tree_t));
+	if (!node)
+		return (NULL);
+
+	node->parent = parent;
+	node->n = value;
+	node->left = node->right = NULL;
+
+	return (node);
+}
